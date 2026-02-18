8e09be5db381cf589865e772425a16cad46bbcd9
sanjay <sanjaynishanth.ss@gmail.com>
2026-02-18 17:34:12 +0530

    Remove .next from repository

--- .next/dev/static/chunks/src_app_layout_tsx_1cf6b850._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_app_globals_91e4631d.css",
-  "static/chunks/src_e9497af2._.js",
-  "static/chunks/node_modules_f1c56a42._.js"
-],
-    source: "dynamic"
-});
